1e03edd4b45c076b0a22e8fa70e7232c33bf5579
HEL Mab <mabhelitc@gmail.com>
2019-06-23 15:55:59 +0700

    clean repository.

--- routes/Backend/Comment.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::group(['prefix', 'Comment'], function () {
-    Route::get('', 'commentsController@index')->name('Comment.index');
-    Route::get('store', 'commentsController@stroe')->name('Comment.store');
-    Route::get('show', 'commentsController@show')->name('Comment.show');
-    Route::get('delete', 'commentsController@delete')->name('Comment.delete');
-});
